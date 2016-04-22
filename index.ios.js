27ae5e4542663e2fdaa25bdc8166616ecf2fbfcc
kat markava <katryc@outlook.com>
2016-04-22 18:46:40 -0400

    render View and styles

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
-'use strict';
+
 var React = require('react-native');
 var Firebase = require('firebase');
 var {
@@ -25,23 +25,24 @@ class todoFirebase extends Component {
 
     this.state = {
       newTodo: '',
-      todoSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row != row2})};
+      todoSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
+    };
 
       this.items = [];
 
 }
    componentDidMount() {
         this.itemsRef.on('child_added', (dataSnapshot) => {
-          this.items.push({id: dataSnapshot.key(), text: dataSnapshot.val()});
+          this.items.push({todo: this.state.newTodo});
           this.setState({
-            todoSource: this.state.todoSource.cloneWithRows(this.itmes)
+            todoSource: this.state.todoSource.cloneWithRows(this.items)
           });
         });
 
         this.itemsRef.on('child_removed', (dataSnapshot) => {
-          this.items = this.items.filter((x) => x.id != dataSnapshot.key());
+          this.items = this.items.filter((x) => x.id !== dataSnapshot.key());
           this.setState({
-            todoSource: this.state.todoSource.cloneWithRows(this.itmes)
+            todoSource: this.state.todoSource.cloneWithRows(this.items)
           });
         });
 
@@ -58,37 +59,110 @@ class todoFirebase extends Component {
      }
    }
 
-   removeTodo() {
-     this.itemsRef.child(rowData.id).remove();
-   }
+   removeTodo(rowData) {
+  this.itemsRef.child(rowData.id).remove();
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          Hello from my second React-native APP
-        </Text>
-      </View>
-    );}
+   return (
+     <View style={styles.appContainer}>
+       <View style={styles.titleView}>
+         <Text style={styles.titleText}>
+           My Todos
+         </Text>
+       </View>
+       <View style={styles.inputcontainer}>
+         <TextInput style={styles.input} onChangeText={(text) => this.setState({newTodo: text})} value={this.state.newTodo}/>
+         <TouchableHighlight
+           style={styles.button}
+           onPress={() => this.addTodo()}
+           underlayColor='#dddddd'>
+           <Text style={styles.btnText}>Add!</Text>
+         </TouchableHighlight>
+       </View>
+       <ListView
+         dataSource={this.state.todoSource}
+         renderRow={this.renderRow.bind(this)} />
+     </View>
+   );
+  }
+
+   renderRow(rowData){
+     return(
+      <TouchableHighlight style={styles.button}
+           underlayColor='#64ffda'
+           onPress={() => this.removeTodo(rowData)}
+           >
+           <View>
+              <View style={styles.row}>
+                <Text style={styles.todoText}>{rowData.text}</Text>
+              </View>
+           </View>
+       </TouchableHighlight>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
+
+var styles = StyleSheet.create({
+  appContainer:{
+    flex: 1
+  },
+  titleView:{
+    backgroundColor: '#48afdb',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  titleText:{
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#64ffda',
-  },
-  welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 100 ,
+  inputcontainer: {
+    marginTop: 5,
+    padding: 10,
+    flexDirection: 'row'
   },
+  button: {
+    height: 36,
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#48afdb',
+    justifyContent: 'center',
+    color: '#FFFFFF',
+    borderRadius: 4,
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+    marginTop: 6,
+  },
+  input: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48afdb',
+    borderRadius: 4,
+    color: '#48BBEC'
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 12,
+    height: 44
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  todoText: {
+    flex: 1,
+  }
 });
 
 AppRegistry.registerComponent('todoFirebase', () => todoFirebase);
