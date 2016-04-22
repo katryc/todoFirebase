6fe61ce067ecd7107c9806dd6e65bd6f1b250ecc
kat markava <katryc@outlook.com>
2016-04-22 14:28:35 -0400

    set-up firebase

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+'use strict'
 import React, {
+  
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  TextInput,
+  ListView
 } from 'react-native';
 
 class todoFirebase extends Component {
@@ -17,14 +16,13 @@ class todoFirebase extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Hello from my second React-native APP
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+
         </Text>
       </View>
     );
